b560fd3b4616bdb04993a998fae1df82a168be08
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-06-11 18:59:13 -0300

    Add project files.

---
+++ MLNetConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNetConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
