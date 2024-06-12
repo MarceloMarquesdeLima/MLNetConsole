479a1edab486c2f6629f164b580d5fb65ec7aba1
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2024-06-11 21:36:26 -0300

    Primeiro projeto

--- MLNetConsole/Program.cs
+++ MLNetConsole/Program.cs
@@ -6,7 +6,7 @@ namespace MLNetConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exemplo01.Exemplo01.Execute();
         }
     }
 }
